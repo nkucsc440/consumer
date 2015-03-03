e65eb5e556c548600b720d6360e1b895a148b666
burtusn <burtousn@gmail.com>
2015-03-03 11:01:44 -0500

    Added Code to iteration1final

---
+++ Deliverables/Iteration 1/Iteration1Final/clickMenu.js
@@ -0,0 +1,109 @@
+///////////////////////////////// Right click menu ///////////////////////////////////////
+var title = "Consume later";
+//Button only appears when right clicking a link
+var id = chrome.contextMenus.create({
+  "title": title,
+  "contexts":['link'],
+  "onclick": handleLink
+});
+//console.log("Link item:" + id);
+
+function handleLink(info, tab) {
+  //console.log(info.linkUrl);
+  saveLink(info.linkUrl);
+}
+
+function stripFragment(url) {
+  return url.split('#')[0];
+}
+
+function saveLink(url, cb) {
+  url = stripFragment(url);
+  chrome.storage.local.get('consumables', function(c){
+    if(!c.consumables)
+      c.consumables = [];
+    if(c.consumables.indexOf(url) === -1) //if the url is not already saved
+      c.consumables.push(url); //save it
+    chrome.storage.local.set({'consumables': c.consumables});//update the storage
+    if(cb)
+      cb();
+  });
+}
+
+//////////////////////////////// Consume Timer Script ///////////////////////////////////////////////
+
+var tabTimes = {};
+var prevTabUrl = '';
+
+//creates the start time
+function startTimer(tabUrl) {
+  var d = new Date();
+  if(!tabTimes[tabUrl])
+    tabTimes[tabUrl] = {};
+  tabTimes[tabUrl].startTime = d.getTime();
+  //console.log(JSON.stringify(tabTimes));
+}
+
+//stops timer and deletes the startTime property
+function stopTimer(tabUrl) {
+  var d = new Date();
+  if(!tabTimes[tabUrl].totalTime)
+    tabTimes[tabUrl].totalTime = 0;
+  tabTimes[tabUrl].totalTime += d.getTime() - tabTimes[tabUrl].startTime;
+  delete tabTimes[tabUrl].startTime;
+  console.log(JSON.stringify(tabTimes));
+}
+
+chrome.tabs.onActivated.addListener(function(activeInfo) {
+  //console.log('onActivated: ' + JSON.stringify(activeInfo));
+  //if active tab is changed and timer is running
+  if(tabTimes[prevTabUrl]) {
+    if(tabTimes[prevTabUrl].startTime) {
+      //stop it
+      stopTimer(prevTabUrl);
+    }
+  }
+  chrome.storage.local.get('consumables', function(c){
+    if(c.consumables) {
+      //may give an error when tabs are closed quickly
+      //doesn't break anything though
+      chrome.tabs.get(activeInfo.tabId, function(tab) {
+        if(c.consumables.indexOf(stripFragment(tab.url)) !== -1) {
+          startTimer(stripFragment(tab.url));
+          prevTabUrl = stripFragment(tab.url);
+        }
+      });
+    }
+  });
+});
+
+chrome.webNavigation.onCommitted.addListener(function(details) {
+  //console.log(JSON.stringify(details));
+  
+  //if going to a new page (in the current tab)
+  chrome.tabs.get(details.tabId, function(tab) {
+    if(details.url && tab.active) {
+      if(stripFragment(details.url) !== prevTabUrl) {
+        
+        //stop the timer on the previous page
+        if(tabTimes[prevTabUrl]) {
+          if(tabTimes[prevTabUrl].startTime) {
+            //stop it
+            stopTimer(prevTabUrl);
+          }
+        }
+        
+        //start the timer for the new page (if saved)
+        chrome.storage.local.get('consumables', function(c){
+          if(c.consumables) {
+            if(c.consumables.indexOf(stripFragment(details.url)) !== -1) {
+              startTimer(stripFragment(details.url));
+            }
+          }
+        });
+        
+        prevTabUrl = stripFragment(details.url);
+      }
+    }
+  });
+});
---
+++ Deliverables/Iteration 1/Iteration1Final/popup.js
@@ -0,0 +1,66 @@
+//gets the url of the current tab and saves the link
+function saveLinks(e) {
+  chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+      //console.log('Saved: ' + tabs[0].url);
+      saveLink(tabs[0].url, window.close);
+  });
+}
+
+//creates a list of all saved links
+//may need to add pages to support lots of links
+function showLinks(e) {
+  var viewDiv = document.getElementById('viewDiv');
+  var linkList;
+  chrome.storage.local.get('consumables', function(c){
+    //console.log(JSON.stringify(c.consumables));
+    linkList = '<ul>\n';
+    linkList += '<li><div><span id="closeLink">Close</span></div></li>\n';
+    for(link in c.consumables) {
+      //console.log(c.consumables[link]);
+      linkList += '<li><div><a href="'+c.consumables[link]+'">'+c.consumables[link]+'</a></div></li>\n';
+    }
+    linkList += '</ul>\n';
+    //console.log(linkList);
+    viewDiv.innerHTML = linkList;
+    document.getElementById('closeLink').addEventListener('click', hideLinks);
+  });
+}
+
+//close the list of links
+function hideLinks(e) {
+  var viewDiv = document.getElementById('viewDiv');
+  viewDiv.innerHTML = '<span id="viewLink">View Consumables</span>';
+  document.getElementById('viewLink').addEventListener('click', showLinks);
+}
+
+function clearConsumables() {
+  chrome.storage.local.clear();
+}
+
+function stripFragment(url) {
+  return url.split('#')[0];
+}
+
+function saveLink(url, cb) {
+  url = stripFragment(url);
+  chrome.storage.local.get('consumables', function(c){
+    if(!c.consumables)
+      c.consumables = [];
+    if(c.consumables.indexOf(url) === -1) //if the url is not already saved
+      c.consumables.push(url); //save it
+    chrome.storage.local.set({'consumables': c.consumables});//update the storage
+    if(cb)
+      cb();
+  });
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  var saveDiv = document.getElementById('saveLink');
+  saveDiv.addEventListener('click', saveLinks);
+  
+  var viewLink = document.getElementById('viewLink');
+  viewLink.addEventListener('click', showLinks);
+  
+  var viewLink = document.getElementById('clearLink');
+  viewLink.addEventListener('click', clearConsumables);
+});
